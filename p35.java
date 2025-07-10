0eb891b897ba1d2a7b6b94759abbebf92b66a311
Hung27091 <thanhhunggtn@gmail.com>
2025-07-10 11:01:25 +0700

    Update p35.java

--- p35.java
+++ p35.java
@@ -1,9 +1,5 @@
 public class p35 {
-    public static void main(String[] args) {
-        int[] a = {1,3,5,6};
-        searchInsert(a,7);
-    }
-    public static int searchInsert(int[] nums, int target) {
+    public  int searchInsert(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             int value = nums[i];
             if (value >= target) {
